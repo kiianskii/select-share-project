8e2d242e84e3297810e2de8266daf286fc3f800b
kiianskii <ankiyanskiy@gmail.com>
2024-08-26 13:26:43 +0300

    added form and map

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const input = document.getElementById("address")! as HTMLInputElement;
+
+const API_KEY = "AIzaSyDcWCG_jl7iiyRuR2gvcw76tYhIwnuJvCQ";
+
+// declare var google: any;
+
+type ApiResTypes = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchHandler(e: Event) {
+  e.preventDefault();
+  const userAddress = input.value;
+  axios
+    .get<ApiResTypes>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        userAddress
+      )}&key=${API_KEY}`
+    )
+    .then((res) => {
+      if (res.data.status !== "OK") {
+        throw new Error("Could not fetch location");
+      }
+      const coordinates = res.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map")!, {
+        center: coordinates,
+        zoom: 12,
+      });
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+
+form.addEventListener("submit", searchHandler);
